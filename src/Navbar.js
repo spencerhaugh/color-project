cfc48a7ed57db489adce7c7981735dc2e7a80ffe
Spencer Haugh <spencer.haugh@gmail.com>
2021-05-26 18:09:42 -0400

    Added MaterialUI select and rgb options

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
 import Slider from 'rc-slider';
 import './Navbar.css';
 import 'rc-slider/assets/index.css';
 
 export default class Navbar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { format: "hex" }
+        this.handleChange = this.handleChange.bind(this);
+    };
+    handleChange(e) {
+        this.setState({ format: e.target.value })
+        this.props.handleChange(e.target.value)
+    }
     render() {
         const { level, changeLevel } = this.props;
         return (
@@ -36,6 +47,13 @@ export default class Navbar extends Component {
                             }}
                         />
                     </div>
+                </div>
+                <div className='select-container'>
+                    <Select value='hex' onChange={this.handleChange}>
+                        <MenuItem value="hex">HEX - #ffffff</MenuItem>
+                        <MenuItem value="rgb">RGB - rgb(255, 255, 255)</MenuItem>
+                        <MenuItem value="rgba">RGBA - rgb(255, 255, 255, 1)</MenuItem>
+                    </Select>
                 </div>
             </nav>
         )
