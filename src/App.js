1b096fb2c7c6dc66639bb8e1e20b8bc796e404a2
Spencer Haugh <spencer.haugh@gmail.com>
2021-05-27 15:33:23 -0400

    MiniPalettes linked to show pages via routeProps history

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   }
   return (
     <Switch>
-      <Route exact path='/' render={() => <PaletteList palettes={seedColors} />} />
+      <Route exact path='/' render={(routeProps) => <PaletteList palettes={seedColors} {...routeProps} />} />
       <Route exact
         path='/palette/:id'
         render={routeProps => (
