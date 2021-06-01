b77aa45b8d3ee9953ca51ff3cd13ce08ad4ee0c6
Spencer Haugh <spencer.haugh@gmail.com>
2021-06-01 15:48:33 -0400

    Style ColorPickerForm

--- src/ColorPickerForm.js
+++ src/ColorPickerForm.js
@@ -1,7 +1,28 @@
 import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import { ChromePicker } from 'react-color';
 import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator';
+
+const styles = {
+    picker: {
+        width: "100% !important",
+        marginTop: "2rem"
+    },
+    addColor: {
+        width: "100%",
+        padding: "1rem",
+        marginTop: "1rem",
+        fontSize: "2rem"
+    },
+    colorNameInput: {
+        width: "100%",
+        height: "70px"
+    },
+    container: {
+        width: "100%"
+    }
+}
 
 class ColorPickerForm extends Component {
     constructor(props) {
@@ -42,35 +63,42 @@ class ColorPickerForm extends Component {
         this.setState({ newColorName: '' })
     };
     render() {
-        const { paletteIsFull } = this.props;
+        const { paletteIsFull, classes } = this.props;
         const { currentColor, newColorName } = this.state;
         return (
-            <div>
+            <div className={classes.container}>
                 <ChromePicker
                     color={currentColor}
                     onChangeComplete={this.updateCurrentColor}
+                    className={classes.picker}
+                    id='picker'
                 />
                 <ValidatorForm onSubmit={this.handleSubmit} >
-                    <Button
-                        variant='contained'
-                        type='submit'
-                        color='primary'
-                        disabled={paletteIsFull}
-                        style={{ backgroundColor: paletteIsFull ? "gray" : currentColor }}
-                    >
-                        {paletteIsFull ? "Palette Full" : "Add Color"}
-                    </Button>
                     <TextValidator
                         value={newColorName}
+                        className={classes.colorNameInput}
+                        variant='filled'
+                        margin="normal"
+                        placeholder="Color Name"
                         name='newColorName'
                         onChange={this.handleChange}
                         validators={['required', 'isColorNameUnique', 'isColorUnique']}
                         errorMessages={['Enter a color name', 'Name must be unique', 'Color already in palette']}
                     />
+                    <Button
+                        variant='contained'
+                        type='submit'
+                        color='primary'
+                        disabled={paletteIsFull}
+                        className={classes.addColor}
+                        style={{ backgroundColor: paletteIsFull ? "gray" : currentColor }}
+                    >
+                        {paletteIsFull ? "Palette Full" : "Add Color"}
+                    </Button>
                 </ValidatorForm>
             </div>
         )
     }
 }
 
-export default ColorPickerForm;
+export default withStyles(styles)(ColorPickerForm);
--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -28,6 +28,8 @@ const useStyles = makeStyles((theme) => ({
     },
     drawerPaper: {
         width: drawerWidth,
+        display: "flex",
+        alignItems: "center"
     },
     drawerHeader: {
         display: 'flex',
@@ -54,6 +56,20 @@ const useStyles = makeStyles((theme) => ({
         }),
         marginLeft: 0,
     },
+    container: {
+        width: "90%",
+        height: "100%",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    buttons: {
+        width: "100%"
+    },
+    button: {
+        width: "50%"
+    }
 }));
 
 // React Functional Component
@@ -142,29 +158,33 @@ export default function NewPaletteForm(props) {
                     </IconButton>
                 </div>
                 <Divider />
-                <Typography variant="h4">Design Your Palette</Typography>
-                <div>
-                    <Button
-                        variant="contained"
-                        color="secondary"
-                        onClick={clearColors}
-                    >
-                        Clear Palette
+                <div className={classes.container}>
+                    <Typography variant="h4" gutterBottom>Design Your Palette</Typography>
+                    <div className={classes.buttons}>
+                        <Button
+                            variant="contained"
+                            color="secondary"
+                            className={classes.button}
+                            onClick={clearColors}
+                        >
+                            Clear Palette
                     </Button>
-                    <Button
-                        variant="contained"
-                        color="primary"
-                        onClick={addRandomColor}
-                        disabled={paletteIsFull}
-                    >
-                        Random Color
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            className={classes.button}
+                            onClick={addRandomColor}
+                            disabled={paletteIsFull}
+                        >
+                            Random Color
                     </Button>
+                    </div>
+                    <ColorPickerForm
+                        paletteIsFull={paletteIsFull}
+                        addNewColor={addNewColor}
+                        colors={colors}
+                    />
                 </div>
-                <ColorPickerForm
-                    paletteIsFull={paletteIsFull}
-                    addNewColor={addNewColor}
-                    colors={colors}
-                />
             </Drawer>
             <main
                 className={clsx(classes.content, {
